17e89a55e913d5a4b7fc594a2bbce57710b5f0cb
Mr.Worldwide <syahfis@gmail.com>
2023-12-25 14:06:38 +0800

    feature: Project Base Settings

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -15,7 +15,9 @@ app.get('/api', (req, res) => {
 });
 
 const port = process.env.PORT || 3333;
+
 const server = app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}/api`);
 });
+
 server.on('error', console.error);
