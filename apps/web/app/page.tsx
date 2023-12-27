c9398d7dc338b0ae4685ead519629cfa66ab402d
Mr.Worldwide <syahfis@gmail.com>
2023-12-27 16:35:05 +0800

    feature: Add Next project && install MUI

---
+++ apps/web/app/page.tsx
@@ -0,0 +1,16 @@
+'use client';
+import { Box, Toolbar, Typography } from '@mui/material';
+export default async function Index() {
+  /*
+   * Replace the elements below with your own.
+   *
+   * Note: The corresponding styles are in the ./index.css file.
+   */
+  return (
+    <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+      <Toolbar />
+      <Typography variant="h3">HOME</Typography>
+      <h3>HOME</h3>
+    </Box>
+  );
+}
